ca7410984df8e0bea75e90c3498f429b4275cb80
Juhi <juhic3162@gmail.com>
2025-01-27 10:32:04 -0500

    Final upcAIcle commit

---
+++ Gemini API Demo/APIKey.swift
@@ -0,0 +1,30 @@
+//
+//  APIKey.swift
+//  Gemini API Demo
+//
+//  Created by Juhi Chitkara on 1/17/25.
+//
+
+import Foundation
+
+enum APIKey {
+    
+    //Etisha Garg Youtube
+    // Fetch the API key from `GenerativeAI-Info.plist`
+    static var `default`: String {
+      guard let filePath = Bundle.main.path(forResource: "GenerativeAi-Info", ofType: "plist")
+      else {
+        fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
+      }
+      let plist = NSDictionary(contentsOfFile: filePath)
+      guard let value = plist?.object(forKey: "API-KEY") as? String else {
+        fatalError("Couldn't find key 'API-KEY' in 'GenerativeAI-Info.plist'.")
+      }
+      if value.starts(with: "_") {
+        fatalError(
+          "Follow the instructions at https://ai.google.dev/tutorials/setup to get an API key."
+        )
+      }
+      return value
+    }
+}
--- Gemini API Demo/ContentView.swift
+++ Gemini API Demo/ContentView.swift
@@ -6,19 +6,72 @@
 //
 
 import SwiftUI
+import GoogleGenerativeAI
 
 struct ContentView: View {
+    let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+    
+    @State var userPrompt = ""
+    @State var response: LocalizedStringKey = "What would you like to upcycle today?"
+    @State var isLoading = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.upcRemovebgPreview)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 25))
+                .padding()
+            ZStack {
+                ScrollView {
+                    Text(response)
+                        .font(.title)
+                        .lineLimit(nil)
+                        
+                }
+                .frame(maxHeight: 300)
+                
+                
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .indigo))
+                        .scaleEffect(4)
+                }
+            }
+            
+            Spacer()
+            
+            TextField("I want to upcycle...", text: $userPrompt, axis: .vertical)
+                .lineLimit(5)
+                .font(.title)
+                .padding()
+                .background(Color.indigo.opacity(0.2), in: Capsule())
+                .disableAutocorrection(true)
+                .onSubmit {
+                    generateResponse()
+                }
         }
         .padding()
+    }
+    
+    func generateResponse(){
+        isLoading = true
+        response = ""
+        
+        Task{
+            do {
+                let result = try await model.generateContent("How can I upcycle "+userPrompt)
+                isLoading = false
+                response = LocalizedStringKey(result.text ?? "No response found")
+                userPrompt = ""
+            } catch {
+                response = "Something went wrong\n\(error.localizedDescription)"
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
